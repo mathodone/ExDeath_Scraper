efa56c7fe4e3f02647ea6f7dab9a381e5ed7b2de
mathodone <mathmorales@uchicago.edu>
2019-05-07 14:19:50 -0700

    Add project files.

---
+++ DataHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExDeath
+{
+    class DataHandler
+    {
+        public static DataTable ReadCsv(string path)
+        {
+            StreamReader sr = new StreamReader(path, System.Text.Encoding.UTF7);
+            string[] headers = sr.ReadLine().Split(',');
+            DataTable dt = new DataTable();
+
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+
+            while (!sr.EndOfStream)
+            {
+                string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                DataRow dr = dt.NewRow();
+
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dr[i] = rows[i];
+                }
+
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+    }
+}
