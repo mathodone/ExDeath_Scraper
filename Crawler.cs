3eb8d9f95c567c2a2ca7fc2db28291f6840288ad
andrew <andrewwhwang@gmail.com>
2019-05-11 01:52:19 -0400

    Added recursion and parallelized crawling

--- Crawler.cs
+++ Crawler.cs
@@ -11,6 +11,12 @@ namespace ExDeath
 {
     public class Crawler
     {
+        // how far down the rabbit hole goes
+        int maxDepth;
+
+        // so children don't loop back to already seen node
+        static HashSet<string> seen = new HashSet<string>();
+
         // list of links to crawl. we obtain this from the root url
         Queue<string> crawlQueue = new Queue<string>();
 
@@ -31,8 +37,9 @@ namespace ExDeath
 
         static string downloadsDirectory;
 
-        public Crawler(string url, bool usekeywords = false, int maxConnections = 2)
+        public Crawler(string url, bool usekeywords = false, int maxConnections = 2, int depth = 2)
         {
+            maxDepth = depth;
             crawlQueue = new Queue<string>();
             uri = new Uri(url);
             client = new HttpClient();
@@ -52,7 +59,7 @@ namespace ExDeath
         
         // gets a list of links from a page and adds them
         // to the crawlQueue
-        public async Task<bool> GenerateQueueAsync()
+        public async Task GenerateQueueAsync()
         {
             try
             {
@@ -76,21 +83,20 @@ namespace ExDeath
                 {
                     // turn relative paths to absolute
                     string fixedlink = link.StartsWith("/") ? $"{uri.Scheme}://{uri.Host}/{link.Substring(1)}" : link;
-
+                    //add it if its new and contains keywords
                     if (!crawlQueue.Contains(fixedlink))
                     {
-                        if (useKeywords)
+                        if (!useKeywords || fixedlink.Split(urlSplit).Intersect(keywords).Any())
                         {
-                            if (fixedlink.Split(urlSplit).Intersect(keywords).Any())
+                            lock (seen)
                             {
-                                crawlQueue.Enqueue(fixedlink);
-                                Logging.QueuedUrl(fixedlink);
+                                if (!seen.Contains(fixedlink))
+                                {
+                                    seen.Add(fixedlink);
+                                    crawlQueue.Enqueue(fixedlink);
+                                    Logging.QueuedUrl(fixedlink);
+                                }
                             }
-                        }
-                        else
-                        {
-                            crawlQueue.Enqueue(fixedlink);
-                            Logging.QueuedUrl(fixedlink);
                         }
                     }
                 }
@@ -102,30 +108,30 @@ namespace ExDeath
                 Console.WriteLine("Exception: ", e);
                 Logging.FailedQueue(uri.ToString());
             }
-
-            return true;
         }
 
         // visit a url and get the source html, then save the html to a file
         // the reason we save html to a file is because we can process the html
         // offline much more quickly
-        public async Task<bool> ProcessUrlAsync(string url, bool saveImages = false)
+        public async Task ProcessUrlAsync(string url, bool saveImages = false)
         {
             Logging.ProcessingNewUrl(url);
 
             try
             {
                 string path = new Uri(url).LocalPath.Substring(1);
-                DirectoryInfo di = Directory.CreateDirectory($"{downloadsDirectory}/{path}/");
+                DirectoryInfo di = Directory.CreateDirectory($"{downloadsDirectory}/{path}");
 
                 HttpResponseMessage response = await client.GetAsync(uri);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
 
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine($"{downloadsDirectory}/{path}/", $"{path}_html.txt")))
+                using (StreamWriter outputFile = new StreamWriter($"{downloadsDirectory}/{path}_html.txt"))
                 {
                     await outputFile.WriteAsync(responseBody);
+                    await outputFile.FlushAsync();
                 }
+
 
                 //if (saveImages)
                 //{
@@ -142,8 +148,6 @@ namespace ExDeath
                 Console.WriteLine("Exception: ", e);
                 Logging.FailedUrl(url);
             }
-
-            return true;
         }
 
         public static void DownloadImages(string html)
@@ -163,7 +167,20 @@ namespace ExDeath
             }
         }
 
-        public async Task<bool> Run()
+        // create child with 1 less depth
+        public async Task Spawn(string url)
+        {
+            await ProcessUrlAsync(url);
+            if (maxDepth > 1)
+            {
+                Crawler crawler = new Crawler(url, depth: maxDepth - 1, 
+                                            usekeywords: useKeywords, 
+                                            maxConnections: ServicePointManager.DefaultConnectionLimit);
+                await crawler.Run();
+            }
+        }
+
+        public async Task Run()
         {
             // clear log file if exists, otherwise make new log file
             File.WriteAllText("../../logs/crawl_log.txt", string.Empty);
@@ -172,8 +189,6 @@ namespace ExDeath
 
             // create new directory to store files
             DirectoryInfo di = Directory.CreateDirectory(downloadsDirectory);
-
-            var result = new List<string>();
 
             runningTasks.Add(GenerateQueueAsync());
 
@@ -186,12 +201,14 @@ namespace ExDeath
                 while (crawlQueue.Any() && runningTasks.Count < ServicePointManager.DefaultConnectionLimit)
                 {
                     var url = crawlQueue.Dequeue();
-                    runningTasks.Add(ProcessUrlAsync(url));
+                    // create recursive child on each crawlQueue
+                    runningTasks.Add(Task.Run(() => Spawn(url)));
                 }
             }
+            await Task.WhenAll(runningTasks);
 
             Logging.CrawlFinished(uri.ToString());
-            return true;
+            //return true;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,13 +9,12 @@ namespace ExDeath
         static void Main(string[] args)
         {
             Console.WriteLine("BEGIN");
-
-            //Crawler crawler = new Crawler("http://www.chinavitae.org/biography_browse.php?l=A", false);
+            Crawler crawler = new Crawler("http://mdn.github.io/beginner-html-site-styled/", depth:2, maxConnections: 3);
+            //Crawler crawler = new Crawler("http://www.chinavitae.org/biography_browse.php?l=A");
             //crawler.LoadKeywords(@"../../keywords/chinavitae_words.txt");
-            //crawler.Run().Wait();
+            crawler.Run().Wait();
 
-            Search.SearchBing("martial arts studio");
-
+            //Search.SearchBing("martial arts studio");
             Console.WriteLine("DONE");
         }
     }
